cf2dfc3a9ba2dd031245b456e5c061f0a36050ff
Juno <juno@Juno-MacBookPro.local>
2021-01-22 11:18:41 +0900

    새로운 DB 설정

--- SpringWeb03Shop/src/main/java/com/juno/shop/dao/ProductDao.java
+++ SpringWeb03Shop/src/main/java/com/juno/shop/dao/ProductDao.java
@@ -9,6 +9,15 @@ import com.juno.shop.dto.Product;
 @Repository
 public class ProductDao {
 
+	// ComboPooledDataSource(스프링컨테이너에 수동으로 넣어놓은 클래스)에서 연결한 Connection을 jdbcTemplate에 전달,
+	// jdbcTemplate을 이용해 DB명령을 실행합니다.
+	private jdbcTemplate template;
+
+	@Autowired
+	public ProductDao(ComboPooledDataSource ds) {
+		this.template = new JdbcTemplate(ds);
+	}
+
 	public ArrayList<Product> getNewList() {
 		return null;
 	}
@@ -16,5 +25,4 @@ public class ProductDao {
 	public ArrayList<Product> getBestList() {
 		return null;
 	}
-
 }
